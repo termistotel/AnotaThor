f317ab9290a8aa59b215eaf4e527c368d0351120
termistotel <termistotel@gmail.com>
2018-10-07 18:19:23 +0200

    Change browse to reload and lay groundwork for displaying and changing images

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import kivy
 kivy.require('1.10.0') # replace with your current kivy version !
 
+import os
 from kivy.app import App
 from pyClasses.landmark import Landmark
+
 
 class AnotathorApp(App):
   def build(self):
@@ -20,9 +22,15 @@ class MainBox(BoxLayout):
   def __init__(self, **kwargs):
     super(MainBox, self).__init__(**kwargs)
 
-    displayLayout = self.ids.display
-    displayLayout.on_touch_down = lambda touch: self.addLandmark(displayLayout, touch)
+    self.previousList = []
+    self.nextList = []
+    self.updateImageList()
 
+    displayLayout = self.ids.display
+    reloadButton = self.ids.reload
+
+    displayLayout.on_touch_down = lambda touch: self.addLandmark(displayLayout, touch)
+    reloadButton.on_click = self.updateImageList
 
   def addLandmark(self, displayLayout, touch):
     x, y = touch.pos
@@ -35,13 +43,20 @@ class MainBox(BoxLayout):
     print(newLandmark.pos)
     print(newLandmark.width)
 
+  def updateImageList(self, *args, **kwargs):
+    self.nextList = os.listdir('images/')
+
 class ToolbarContainer(BoxLayout):
   pass
 
 class DisplayLayout(FloatLayout):
-  pass
   # TODO: Implement changable size of landmarks
   # landmarkScale = NumericProperty(0.1)
+
+  def changeImg(self, src):
+    if src:
+      self.clear_widgets()
+      self.add_widget(Image(source=src))
 
 if __name__ == '__main__':
     AnotathorApp().run()
