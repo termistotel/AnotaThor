d1a51506d4652989588bca1f6c3e2792f0e78f2c
termistotel <termistotel@gmail.com>
2018-10-07 17:00:40 +0200

    Make landmarks apear on simple click

--- main.py
+++ main.py
@@ -16,15 +16,33 @@ from kivy.uix.widget import Widget
 from kivy.uix.behaviors import DragBehavior
 from kivy.properties import NumericProperty
 
+
 class MainBox(BoxLayout):
   def __init__(self, **kwargs):
     super(MainBox, self).__init__(**kwargs)
+
+    displayLayout = self.ids.display
+    displayLayout.on_touch_down = lambda touch: self.addLandmark(displayLayout, touch)
+
+
+  def addLandmark(self, displayLayout, touch):
+    x, y = touch.pos
+
+    newLandmark = Landmark()
+    displayLayout.add_widget(newLandmark)
+    newLandmark.center = (x,y)
+
+    print(newLandmark.center)
+    print(newLandmark.pos)
+    print(newLandmark.width)
 
 class ToolbarContainer(BoxLayout):
   pass
 
 class DisplayLayout(FloatLayout):
   pass
+  # TODO: Implement changable size of landmarks
+  # landmarkScale = NumericProperty(0.1)
 
 class Landmark(DragBehavior, Widget):
   def __init__(self, **kwargs):
