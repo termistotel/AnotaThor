90396c5beb70e420d61b8fb807ca0a808a6e9955
termistotel <termistotel@gmail.com>
2018-10-07 21:43:13 +0200

    Add switching for insertion and dragging, remove some debugging code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kivy
 kivy.require('1.10.0') # replace with your current kivy version !
 
+from functools import partial
 import os
 from kivy.app import App
 from pyClasses.landmark import Landmark
@@ -18,7 +19,6 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import NumericProperty
 
-
 class MainBox(BoxLayout):
   def __init__(self, **kwargs):
     super(MainBox, self).__init__(**kwargs)
@@ -27,26 +27,39 @@ class MainBox(BoxLayout):
     self.nextList = []
     self.updateImageList()
 
+    # References to main widgets
     displayLayout = self.ids.display
     reloadButton = self.ids.reload
     nextButton = self.ids.next
     prevButton = self.ids.prev
+    dragButton = self.ids.drag
+    insertButton = self.ids.insert
 
-    displayLayout.on_touch_down = lambda touch: self.addLandmark(displayLayout, touch)
+    # Two functions of display
+    dragFunction = displayLayout.on_touch_down
+    insertFunction = self.addLandmark
+
+    # Button operations
     reloadButton.on_press = self.updateImageList
     nextButton.on_press = self.nextImage
     prevButton.on_press = self.prevImage
+    dragButton.on_press = partial(self.changeMouseFunction, dragFunction)
+    insertButton.on_press = partial(self.changeMouseFunction, insertFunction)
 
-  def addLandmark(self, displayLayout, touch):
+    # Starting states
+    insertButton.state = "down"
+    displayLayout.on_touch_down = insertFunction
+
+  def addLandmark(self, touch, *args, **kwargs):
+    displayLayout = self.ids.display
     x, y = touch.pos
 
     newLandmark = Landmark()
     displayLayout.add_widget(newLandmark)
     newLandmark.center = (x,y)
 
-    print(newLandmark.center)
-    print(newLandmark.pos)
-    print(newLandmark.width)
+  def changeMouseFunction(self, function, *args, **kwargs):
+    self.ids.display.on_touch_down = function
 
   def updateImageList(self, *args, **kwargs):
     self.nextList = os.listdir('images/')
@@ -54,20 +67,15 @@ class MainBox(BoxLayout):
     self.nextList.sort(reverse=True)
     self.ids.display.changeImg('images/' + self.nextList[len(self.nextList)-1])
 
-
   def nextImage(self, *args, **kwargs):
     if len(self.nextList) > 1:
       self.previousList.append(self.nextList.pop())
       self.ids.display.changeImg('images/' + self.nextList[len(self.nextList)-1])
 
-    print('images/' + self.nextList[len(self.nextList)-1])
-
   def prevImage(self, *args, **kwargs):
     if len(self.previousList) >=1:
       self.nextList.append(self.previousList.pop())
       self.ids.display.changeImg('images/' + self.nextList[len(self.nextList)-1])
-
-    print('images/' + self.nextList[len(self.nextList)-1])
 
 class ToolbarContainer(BoxLayout):
   pass
